1be63d5c944c5ae2d9af056d5b5095be0a6c5f0e
unknown <lpsk5713@gmail.com>
2021-10-21 16:24:47 +0530

    Video Footer with Disc Completed

--- src/VideoFooter.js
+++ src/VideoFooter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./videoFooter.css"
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import Ticker from "react-ticker";
 
 function VideoFooter() {
     return (
@@ -7,7 +9,21 @@ function VideoFooter() {
             <div className="videoFooter__text">
                 <h3>@lpsk</h3>
                 <p>This is the description.....</p>
+                <div className="videoFooter__ticker">
+                    <MusicNoteIcon className="videoFooter__icon" />
+                    <Ticker mode="smooth">
+                        {({ index }) => (
+                            <>
+                                <p>
+                                    I am a song
+                                </p>
+                            </>
+                        )}
+                    </Ticker>
+                </div>
             </div>
+            <img src="https://lh3.googleusercontent.com/proxy/JsaMUfFTkFGanC1H--d_Suc8tJleF7jzoP8alZDBVLYkfKvOD9tyjKs7ldzQAdtGXfSZT_t6mzXkQgdJADVUM-tO"
+                alt="" className="videoFooter__record" />
         </div>
     )
 }
