249efff8972bf9a2ce16dbba06a02b664caed6a9
unknown <lpsk5713@gmail.com>
2021-10-21 15:51:31 +0530

    VideoFooter Username @ and Discription with css is added

--- src/Video.js
+++ src/Video.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import "./Video.css"
+import { useRef } from 'react';
+import { useState } from 'react';
+import VideoFooter from './VideoFooter';
 
 function Video() {
+    const [playing, setPlaying] = useState(false);
+    const videoRef = useRef(null);
+    const handleVideoPress = () => {
+        // Stop the video if Playing
+        if (playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+        // Otherwise Play the video
+    }
     return (
-        <div className = "video">
+        <div className="video">
             <video
-            className = "video__player" 
-            loop 
-            src="https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4"></video>
-            {/* VideoFotter */}
+                className="video__player"
+                ref={videoRef}
+                onClick={handleVideoPress}
+                loop
+                src="https://assets.mixkit.co/videos/preview/mixkit-portrait-of-a-woman-in-a-pool-1259-large.mp4">
+            </video>
+            <VideoFooter />
         </div>
     )
 }
---
+++ src/VideoFooter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./videoFooter.css"
+
+function VideoFooter() {
+    return (
+        <div className="videoFooter">
+            <div className="videoFooter__text">
+                <h3>@lpsk</h3>
+                <p>This is the description.....</p>
+            </div>
+        </div>
+    )
+}
+
+export default VideoFooter
